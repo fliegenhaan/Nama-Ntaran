480cf4f3a4b62df9bb701b5a8e088aee92add515
Muhammad Raihaan Perdana <perdanaraihan96@gmail.com>
2025-11-28 21:34:14 +0700

    Deployment to alchemy

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -22,6 +22,13 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+      gasPrice: 20000000000, // 20 gwei
+      timeout: 60000 // 60 seconds timeout
+    },
     polygonMumbai: {
       url: process.env.POLYGON_MUMBAI_RPC || "https://rpc-mumbai.maticvigil.com",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
